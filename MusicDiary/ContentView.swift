09e9f00611627cdccf529a230e602d57eafc7419
kanato1221 <kanatonton2@gmail.com>
2026-06-21 13:50:11 +0900

    Initial Commit

---
+++ MusicDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicDiary
+//
+//  Created by 松井奏人 on 2026/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicDiary/MusicDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicDiaryApp.swift
+//  MusicDiary
+//
+//  Created by 松井奏人 on 2026/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
